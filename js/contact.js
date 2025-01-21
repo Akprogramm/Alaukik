9ef213db962d986e3f98318c41d9219e6fd6a33b
abdul kadar khan <monukadar12345@gmail.com>
2025-01-21 11:36:35 +0530

    second commit

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,18 @@
+
+(function ($) {
+  "use strict";
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 45) {
+      $('.navbar').addClass('sticky-top shadow-sm');
+  } else {
+      $('.navbar').removeClass('sticky-top shadow-sm');
+  }
+}); 
+
+})(jQuery);
+
+
 document.getElementById("sub").addEventListener("click",function(event){
     event.preventDefault();
 
@@ -25,5 +40,23 @@ document.getElementById("sub").addEventListener("click",function(event){
      document.getElementById("subject").value = '';
      document.getElementById("message").value = '';
 
-        console.log("clicked");
-})
+    })
+    
+    
+function checkregx(){ 
+  let sub = document.getElementById("sub");
+ let names = document.getElementById("name").value;
+ let email = document.getElementById("email").value;
+ let ContactNo = document.getElementById("ContactNo").value;
+ let subject = document.getElementById("subject").value; 
+ let message = document.getElementById("message").value;
+
+ let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+   if( names != '' && regex.test(email) && ContactNo.length==10 && subject != '' && message != ''){
+    sub.disabled = false; 
+   } 
+   else{
+    sub.disabled = true;
+   }
+}
--- js/policy.js
+++ js/policy.js
@@ -1,7 +1,20 @@
+(function ($) {
+    "use strict";
+  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+        $('.navbar').addClass('sticky-top shadow-sm');
+    } else {
+        $('.navbar').removeClass('sticky-top shadow-sm'); 
+    } 
+  }); 
+  
+  })(jQuery);
+
 function check(){
 
     setTimeout(() => {
-        window.location.href = "index.html";
+        window.location.href = "index.html"; 
     }, 2000); 
 
 }
